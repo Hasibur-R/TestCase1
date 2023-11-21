5c29d174d4192472b8eee114b1965141c38252f5
Md. Hasibur Rahman <connect.hasibur@gmail.com>
2023-11-21 22:49:50 +0600

    some changed

--- src/test/java/test/CookieTest.java
+++ src/test/java/test/CookieTest.java
@@ -11,7 +11,6 @@ public class CookieTest extends BaseTest {
 
     @Test
     public void verifyCookieTest() {
-        AqualityServices.getBrowser().maximize();
         AqualityServices.getBrowser().goTo(EnvironmentConfig.getUrl());
         AqualityServices.getBrowser().waitForPageToLoad();
 
